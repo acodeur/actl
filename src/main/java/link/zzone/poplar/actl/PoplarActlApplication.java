2285089876e0e86476211ab685b4a1ce6556951f
Chris Chen <y_zhengchen@163.com>
2024-03-01 18:18:41 +0800

    first commit for initial framework

---
+++ src/main/java/link/zzone/poplar/actl/PoplarActlApplication.java
@@ -0,0 +1,13 @@
+package link.zzone.poplar.actl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoplarActlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoplarActlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/link/zzone/poplar/actl/PoplarActlApplicationTests.java
@@ -0,0 +1,13 @@
+package link.zzone.poplar.actl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoplarActlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
