c99d9e2b247686142ece8fa7b6f3e00ab4a83841
acodeur <ccyzhope@gmail.com>
2024-03-02 11:25:34 +0800

    feat: add picocli-codegen for native-image generation

---
+++ src/main/java/link/zzone/poplar/actl/PoplarActlCmd.java
@@ -0,0 +1,25 @@
+package link.zzone.poplar.actl;
+
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author chrischen
+ */
+@CommandLine.Command(name = "actl", description = "Actl ConTroL for poplar")
+public class PoplarActlCmd implements Callable<String> {
+
+    public static void main(String[] args) {
+        CommandLine cmd = new CommandLine(new PoplarActlCmd());
+        int exitCode = cmd.execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public String call() throws Exception {
+        String msg = "poplar-actl using picocli";
+        System.out.println(msg);
+        return "poplar-actl using picocli";
+    }
+}
---
+++ src/test/java/link/zzone/poplar/actl/PoplarActlCmdTests.java
@@ -0,0 +1,9 @@
+package link.zzone.poplar.actl;
+
+class PoplarActlCmdTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
